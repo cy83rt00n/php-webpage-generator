ab53135dea75b7dc265ca66be56e2eff872d0bdc
Artem Tsybin <cy83rt00n@gmail.com>
2019-05-02 09:07:48 +0300

    add WebPage function

--- lib/Loader.php
+++ lib/Loader.php
@@ -3,8 +3,14 @@
 namespace WebPageGenerator;
 
 use WebPageGenerator\HTMLTag\Tag;
+use WebPageGenerator\WebPage\WebPage;
 
 function HTMLTag($name, $content='', $id='', $class='', $data=array())
 {
-    return new Tag($name, $content='', $id='', $class='', $data=array());
+    return new Tag($name, $content, $id, $class, $data);
+}
+
+function WebPage()
+{
+    return new WebPage();
 }
